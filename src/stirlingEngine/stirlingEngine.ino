842e03fd3f492edb51512d0cc8bf04ec4bb03fb7
clnbtlr <125999934+clnbtlr@users.noreply.github.com>
2023-10-03 15:03:23 +0100

    First working AK7451 code
    Changed tx_buf and SPI_MODE2 and now shows angle values when magnet is close

--- src/stirlingEngine/stirlingEngine.ino
+++ src/stirlingEngine/stirlingEngine.ino
@@ -26,27 +26,25 @@ void loop() {
   uint8_t reg = 0x00; // register R_ANG
   uint8_t opcode = 0x09; // opcode READ_ANGLE
 
-  uint8_t tx_buf[1] = {0};
+  uint8_t tx_buf = 0;
   uint8_t rx_buf[2] = {0,0};
   uint16_t raw_data = 0;
   reg <<= 1; // register address is 7 bits
-  tx_buf[ 0 ] = ( opcode << 4 ) | ( reg >> 4);
-  //Serial.println(tx_buf[0], BIN);
+  tx_buf = ( opcode << 4 ) | ( reg >> 4 );
+  Serial.println(tx_buf, BIN);
 
   SPI.beginTransaction(SPISettings(2000000, MSBFIRST, SPI_MODE2));
   digitalWrite(CS, LOW); // enable Chip Select
-  delay(10);
   SPI.transfer(tx_buf); 
   rx_buf[0] = SPI.transfer(0);
   rx_buf[1] = SPI.transfer(0);
-  delay(10);
   digitalWrite(CS, HIGH); // disable Chip Select
   SPI.endTransaction();
 
   raw_data = rx_buf[0];
   raw_data <<=8;
   raw_data |= rx_buf[1];
-  raw_data &= 0x0FFF;
+  raw_data &= 0x0FFF; // angle data is 12 bits
   Serial.print((uint16_t)raw_data, BIN);
   float angle = (360*(float)raw_data)/4096;
   Serial.print(", ");Serial.println(angle);
